287e794d1e1063db0610e1eabdebe697fecf9181
jhwang <jhwang@paas.local>
2019-07-16 16:11:46 +0900

    all

--- src/main/kotlin/me/jhwang/cloudstream_kafka/Model.kt
+++ src/main/kotlin/me/jhwang/cloudstream_kafka/Model.kt
@@ -6,3 +6,11 @@ data class Note (
         var message: String,
         var location: String = ""
 )
+
+data class Todo (
+        var id: String = "",
+        var title: String,
+        var message: String,
+        var schedule: Long,
+        var location: String = ""
+)
